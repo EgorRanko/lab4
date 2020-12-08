fa4fb541106415a423b482c0e5770ba4ec83bc7e
EgorRanko <egor2410011q@gmail.com>
2020-12-08 13:42:25 +0500

    lab4

--- lab4.R
+++ lab4.R
@@ -1,7 +1,7 @@
 f <- function(){
   setwd("E:/R")
   v1 <- read.table("lab.txt", header = TRUE)
-  v2 <- scan("", sep = ',', what = "")
+  v2 <- readline("¬ведите: ")
   v3 <- data.frame(v1, v2)
   print(v3)
 }
